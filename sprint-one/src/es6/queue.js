0041b306b8b60a113e6156cb122cedc00c676456
Jeremy Sim <59082457+jsim0809@users.noreply.github.com>
2020-02-05 13:15:43 -0800

    Finished Sprint 1.

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,31 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.currentIndex = 0;
+  }
+
+  enqueue(value) {
+    this.storage[this.currentIndex] = value;
+    this.currentIndex++;
+  }
+
+  dequeue() {
+    var check = 0;
+    if (this.size()) {
+      while (this.storage[check] === undefined) {
+        check++;
+      }
+      var result = this.storage[check];
+      delete this.storage[check];
+      return result;
+    } else {
+      return undefined;
+    }
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
   }
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,31 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.currentIndex = 0;
+  }
+
+  push(value) {
+    this.storage[this.currentIndex] = value;
+    this.currentIndex++;
+  }
+
+  pop() {
+    var check = this.currentIndex;
+    if (this.size()) {
+      while (this.storage[check] === undefined) {
+        check--;
+      }
+      var result = this.storage[check];
+      delete this.storage[check];
+      return result;
+    } else {
+      return undefined;
+    }
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
   }
 
 }
