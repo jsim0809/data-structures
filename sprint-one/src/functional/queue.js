12bc7794c7c704ce1e08cda63929639650dc7362
Jeremy Sim <59082457+jsim0809@users.noreply.github.com>
2020-02-05 12:15:02 -0800

    Finished functional.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,35 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var currentKey = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[currentKey] = value;
+    currentKey++;
   };
 
   someInstance.dequeue = function() {
+    var check = 0;
+    if (Object.keys(storage).length) {
+      while (storage[check] === undefined) {
+        check++;
+      }
+      var result = storage[check];
+      delete storage[check];
+      return result;
+    } else {
+      return undefined;
+    }
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
+  };
+
+  someInstance.debug = function() {
+    return storage;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,30 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var currentKey = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[currentKey] = value;
+    currentKey++;
   };
 
   someInstance.pop = function() {
+    var check = currentKey;
+    if (Object.keys(storage).length) {
+      while (storage[check] === undefined) {
+        check--;
+      }
+      var result = storage[check];
+      delete storage[check];
+      return result;
+    } else {
+      return undefined;
+    }
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
