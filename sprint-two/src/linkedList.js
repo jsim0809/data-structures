9a63b9b05018016eaa16cbc702bbe5dfaebeb899
jsim0809 <jeremy_sim@hotmail.com>
2020-02-05 16:23:30 -0800

    Add justification for LinkedList methods

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,13 +2,15 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
-
+  // Appends value to tail and makes it the new tail
   list.addToTail = function(value) {
   };
-
+  // Pops off the head and returns its value.
+  // The second value is now the head
   list.removeHead = function() {
   };
-
+  // Returns a boolean representing if
+  // target is in linked list.
   list.contains = function(target) {
   };
 
