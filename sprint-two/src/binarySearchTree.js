25a98df78660725ab91753eaeaf6e83a918feafe
Jin Cai <jinruncai@gmail.com>
2020-02-07 21:27:26 -0800

    Implement BinarySearchTree's insert method

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -10,12 +10,36 @@ var BinarySearchTree = function(value) {
 
 };
 
+// Takes a value and sorts it into the binary search tree
+BinarySearchTree.prototype.insert = function(value) {
+  // Creates a new BinarySearchTree out of the given value
+  var leaf = BinarySearchTree(value);
 
-BinarySearchTree.prototype.insert = function() {
+  // Checks if the value is smaller than the current BST's value
+  // if smaller
+  if (value < this.value) {
 
-};
-
-BinarySearchTree.prototype.contains = function() {
+    // check if left exists
+    if (this.left === undefined) {
+      // if it does not, set the left property to the new BST
+      this.left = leaf;
+      // else, have the left node run insert(value).
+    } else {
+      this.left.insert(value);
+    }
+    // else if larger
+  } else if (value > this.value) {
+    // check if right exists
+    if (this.right === undefined) {
+      // if it does not, set the right property to the new BST
+      this.right = leaf;
+    } else {
+      // else, have the right node run insert(value).
+      this.right.insert(value);
+    }
+  }
+  // else (if equal)
+  // don't do anything.
 
 };
 
@@ -28,3 +52,4 @@ BinarySearchTree.prototype.depthFirstLog = function() {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
